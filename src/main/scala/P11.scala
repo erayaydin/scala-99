0fe16ccd90bbf5f7d1866071b100e916bf598682
Eray Aydın <erayaydinn@protonmail.com>
2024-05-15 12:45:18 +0300

    feat: add problem description to p11 object

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -2,6 +2,13 @@ package in.yayd.era.s99
 
 import P10.encode
 
+/** Modified run-length encoding.
+  *
+  * {{{
+  * scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  * res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+  * }}}
+  */
 object P11 {
   def encodeModified[A](list: List[A]): List[Either[A, (Int, A)]] = {
     encode(list)
